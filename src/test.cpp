4b925ed86cd794e6e6c3aed8391e8b264f258561
yonidavidson <yoni.davidson@tg-17.com>
2018-07-07 14:27:49 +0300

    multiple functions

--- src/test.cpp
+++ src/test.cpp
@@ -1,35 +1,53 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
 #include "catch.hpp"
 #include "lib.h"
 
-int my_function(char arguments[]){
+int function1(char *arguments) {
     return atoi(arguments);
 }
 
-TEST_CASE( "Check default Command line parser" ) {
-    char key[] = "my_command";
-    char arguments[]= "123";
-    char description[] = "sample function description";
-    handler_function f = my_function;
-    CommandHandler *uut = new CommandHandler();
-	int add_response = uut->add(key, f, description);
-
-	int response = uut->exec(key, arguments);
-
-	REQUIRE(add_response == 0);
-    REQUIRE( response == 123 );
-    REQUIRE(!strcmp( description, uut->description(key)));
+int function2(char *arguments) {
+    return atoi(arguments)*2;
 }
 
-TEST_CASE( "Check Command" ) {
+TEST_CASE("Check one function working") {
     char key[] = "my_command";
-    char arguments[]= "123";
+    char arguments[] = "123";
     char description[] = "sample function description";
-    handler_function f = my_function;
-    Command *uut = new Command(f,description,key);
+    handler_function f = function1;
+    CommandHandler *uut = new CommandHandler();
+    int add_response = uut->add(key, f, description);
 
-    int response = uut->exec(arguments);
+    int exec_response = uut->exec(key, arguments);
 
-	REQUIRE( response == 123 );
-    REQUIRE(!strcmp( description, uut->description()));
+    REQUIRE(add_response == 0);
+    REQUIRE(exec_response == 123);
+    REQUIRE(!strcmp(description, uut->description(key)));
+}
+
+TEST_CASE("Check multiple functions execution") {
+    char key1[] = "function1";
+    char description1[] = "function1";
+    handler_function f1 = function1;
+    char key2[] = "function2";
+    char description2[] = "function2";
+    handler_function f2 = function2;
+
+    char arguments[] = "123";
+
+    CommandHandler *uut = new CommandHandler();
+    int add_response1 = uut->add(key1, f1, description1);
+    int add_response2 = uut->add(key2, f2, description2);
+
+    int exec_response1 = uut->exec(key1, arguments);
+    int exec_response2 = uut->exec(key2, arguments);
+
+    REQUIRE(add_response1 == 0);
+    REQUIRE(exec_response1 == 123);
+    REQUIRE(!strcmp(description1, uut->description(key1)));
+
+    REQUIRE(add_response2 == 0);
+    REQUIRE(exec_response2 == 246);
+    REQUIRE(!strcmp(description2, uut->description(key2)));
 }
